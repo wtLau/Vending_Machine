6e2926f02339456e59705357d75f211699bf1a43
Brian Lau <lauwangtatbrian@gmail.com>
2017-08-20 19:13:18 -0700

    add basic functionality for refilling inventory

---
+++ api/payload.js
@@ -0,0 +1,62 @@
+module.exports = {
+    products: [
+        {
+            id: 1,
+            product: "potato_chip",
+            price: "350",
+            quantity: 9
+        },
+        {
+            id: 2,
+            product: "cookies",
+            price: "170",
+            quantity: 4
+        },
+        {
+            id: 3,
+            product: "BBQ_chip",
+            price: "245",
+            quantity: 0
+        },
+        {
+            id: 4,
+            product: "chocolate_bar",
+            price: "055",
+            quantity: 11
+        }
+    ],
+    coinBalance: [
+        {
+            Nickel: {
+                id: 1,
+                type: "Nickel",
+                value: "005",
+                quantity: 20,
+            },
+            Dime: {
+                id: 2,
+                type: "Dime",
+                value: "010",
+                quantity: 40,
+            },
+            Quarter: {
+                id: 3,
+                type: "Quarter",
+                value: "025",
+                quantity: 2,
+            },
+            Loonie: {
+                id: 4,
+                type: "Loonie",
+                value: "100",
+                quantity: 9,
+            },
+            Toonie: {
+                id: 5,
+                type: "Toonie",
+                value: "200",
+                quantity: 5,
+            }
+        }
+    ]
+}
--- tests/vending_machine.test.js
+++ tests/vending_machine.test.js
@@ -2,19 +2,87 @@ const vending_machine = require('../lib/vending_machine')
 
 
 describe ('vending_machine', () => {
-    xit('should printing inventory', () => {
-        
+
+    describe ('Customer Experience', () => {
+        it('should printing inventory', () => {
+            const expected = [
+                {
+                    "id": 1,
+                    "product": "potato_chip",
+                    "price": "350",
+                    "quantity": 9
+                },
+                {
+                    "id": 2,
+                    "product": "cookies",
+                    "price": "170",
+                    "quantity": 4
+                },
+                {
+                    "id": 3,
+                    "product": "BBQ_chip",
+                    "price": "245",
+                    "quantity": 0
+                },
+                {
+                    "id": 4,
+                    "product": "chocolate_bar",
+                    "price": "055",
+                    "quantity": 11
+                }
+            ]
+            const inventoryData = new vending_machine()
+            const received = inventoryData.printInventory()
+            expect(received).toEqual(expected);
+        });
+        xit('should be dispensing inventory based on payment', () => {
+            
+        });
+        xit('should returning change as coins (eg. $0.35 is 1 quarter and 1 dime)', () => {
+            
+        });
+        xit('Dispense the smallest amount of coins when returning change)', () => {
+            
+        });
     });
-    xit('should refilling inventory', () => {
-        
+
+    describe ('Machine Maintenance', () => {
+        it('should refilling inventory for each product', () => {
+            // 9,4,0,11            
+            const expected = [
+                {
+                    "id": 1,
+                    "product": "potato_chip",
+                    "price": "350",
+                    "quantity": 10
+                },
+                {
+                    "id": 2,
+                    "product": "cookies",
+                    "price": "170",
+                    "quantity": 5
+                },
+                {
+                    "id": 3,
+                    "product": "BBQ_chip",
+                    "price": "245",
+                    "quantity": 1
+                },
+                {
+                    "id": 4,
+                    "product": "chocolate_bar",
+                    "price": "055",
+                    "quantity": 12
+                }
+            ]
+            const inventoryData = new vending_machine()
+            const received = inventoryData.refillingInventory(1)
+            expect(received).toEqual(expected);
+        });
+        xit('should re-supplying change', () => {
+            
+        });
     });
-    xit('should re-supplying change', () => {
-        
-    });
-    xit('should be dispensing inventory based on payment', () => {
-        
-    });
-    xit('should returning change as coins (eg. $0.35 is 1 quarter and 1 dime)', () => {
-        
-    });
+
 });
+
